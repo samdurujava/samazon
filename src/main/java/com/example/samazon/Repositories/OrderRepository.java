b9356e9a14a70f1749aa03fbe0c2ad687927edc2
Justin <justinyang1000@hotmail.com>
2019-08-07 14:55:39 -0400

    Shipping Page

--- src/main/java/com/example/samazon/Repositories/OrderRepository.java
+++ src/main/java/com/example/samazon/Repositories/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.example.samazon.Repositories;
 
 import com.example.samazon.Beans.Order;
+import com.example.samazon.Beans.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    Order findByUser(User user);
 }
